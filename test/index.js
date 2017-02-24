02789a81a63175228b020a7c179f8dce6c45a724
Bernard Ojengwa <bernardojengwa@gmail.com>
2017-02-24 14:45:19 +0100

    update test test suite
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- test/index.js
+++ test/index.js
@@ -1,4 +1,19 @@
 'use strict'
 
 const should = require('should')
-const dao = require('../lib/dao')
+const DAO = require('../lib/dao')
+
+describe('General test cases for DAO', function () {
+
+	describe('creates an instance correctly', function () {
+		let dao = DAO()
+
+		it('should be an Object', function () {
+			dao.should.be.Object()
+		})
+
+		it('should be instance of DAO', function () {
+			dao.should.be.instanceof(DAO)
+		})
+	})
+})
